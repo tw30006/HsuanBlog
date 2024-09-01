6463b6c26410172fee0bdfdcd9db24780f783121
tw30006 <original.heart.2016@gmail.com>
2024-09-01 15:11:24 +0800

    add: install bcrypt.js for encrypted password

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const app = express();
 const cors = require('cors');
 const port = 4000
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User')
 const dotenv = require('dotenv');
 
 dotenv.config();
-
-
 mongoose.connect(process.env.MONGO_URL);
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +18,9 @@ app.use(express.json());
 app.post('/register', async(req, res)=>{
   const {username,password,name} =req.body;
   try{
-    const userDoc = await User.create({username, password, name})
+    const userDoc = await User.create({username, 
+      password: bcrypt.hashSync("password",salt),
+       name})
     res.json(userDoc);
   } catch(e) {
     res.status(400).json(e);
